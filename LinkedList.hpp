c7935e58b25a6fda5f5d3d921336fcfa0a302932
29thnight <ideneb@naver.com>
2024-11-11 13:32:50 +0900

    초기 커밋.

---
+++ LinkedList.hpp
@@ -0,0 +1,126 @@
+#pragma once
+#include <functional>
+//사용법은 리스트에 담고 싶은 객체에 LinkProperty를 상속받아서 사용하면 됩니다.
+template <typename _Type>
+class LinkedList;
+
+template <typename _Type>
+struct LinkProperty
+{
+	LinkProperty* pPrv{ nullptr };
+	LinkProperty* pNext{ nullptr };
+	_Type* pElement{ nullptr };
+
+public:
+	LinkProperty() = default;
+	virtual ~LinkProperty() = default;
+
+private:
+	friend class LinkedList<_Type>;
+
+private:
+	void InternalSeparate()
+	{
+		pPrv = nullptr;
+		pNext = nullptr;
+	}
+};
+//링크드 리스트 클래스입니다. 해당 클래스로 링크, 언링크 가능하고, ForEach로 순회작업을 할 수 있습니다.
+template <typename _Type>
+class LinkedList
+{
+public:
+	void Link(LinkProperty<_Type>* pNewLinkProperty)
+	{
+		if (!pHead)
+		{
+			pHead = pNewLinkProperty;
+			pTail = pNewLinkProperty;
+		}
+		else
+		{
+			pNewLinkProperty->pPrv = pTail;
+			pTail->pNext = pNewLinkProperty;
+			pTail = pNewLinkProperty;
+		}
+	}
+
+	void Unlink(LinkProperty<_Type>* pDeleteLinkProperty)
+	{
+		LinkProperty<_Type>*& LinkageFront = pDeleteLinkProperty->pPrv;
+		LinkProperty<_Type>*& LinkageBack = pDeleteLinkProperty->pNext;
+
+		if (LinkageFront)
+		{
+			LinkageFront->pNext = LinkageBack;
+		}
+		else
+		{
+			pHead = LinkageBack;
+		}
+
+		if (LinkageBack)
+		{
+			LinkageBack->pPrv = LinkageFront;
+		}
+		else
+		{
+			pTail = LinkageFront;
+		}
+
+		pDeleteLinkProperty->InternalSeparate();
+	}
+
+	void ClearLink()
+	{
+		LinkProperty<_Type>* pCurrent = pHead;
+		while (pCurrent)
+		{
+			LinkProperty<_Type>* pNext = pCurrent->pNext;
+			pCurrent->pPrv = nullptr;
+			pCurrent->pNext = nullptr;
+			pCurrent = pNext;
+		}
+		pHead = nullptr;
+		pTail = nullptr;
+	}
+
+	LinkProperty<_Type>* GetHead()
+	{
+		return pHead;
+	}
+
+	LinkProperty<_Type>* GetTail()
+	{
+		return pTail;
+	}
+
+	LinkProperty<_Type>* GetNext(LinkProperty<_Type>* pCurrent)
+	{
+		return pCurrent->pNext;
+	}
+
+	LinkProperty<_Type>* GetPrv(LinkProperty<_Type>* pCurrent)
+	{
+		return pCurrent->pPrv;
+	}
+
+	_Type* GetElement(LinkProperty<_Type>* pCurrent)
+	{
+		return pCurrent->pElement;
+	}
+
+	void ForEach(auto func)
+	{
+		LinkProperty<_Type>* pCurrent = pHead;
+		while (pCurrent)
+		{
+			func(pCurrent->pElement);
+			pCurrent = pCurrent->pNext;
+		}
+	}
+
+private:
+	LinkProperty<_Type>* pHead{};
+	LinkProperty<_Type>* pTail{};
+};
---
+++ LinkedListLibTest.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include "LinkedList.hpp"
+
+class Test : public LinkProperty<Test>
+{
+public:
+	Test(int _Value) : Value(_Value)
+	{
+		pElement = this;
+		std::cout << "Test()" << std::endl;
+	}
+	int Value;
+
+	bool operator== (const Test& Other) const
+	{
+		return Value == Other.Value;
+	}
+};
+
+int main()
+{
+	LinkedList<Test> List;
+
+	Test* pInt1 = new Test(1);
+	Test* pInt2 = new Test(2);
+	Test* pInt3 = new Test(3);
+
+	List.Link(pInt1);
+	List.Link(pInt2);
+	List.Link(pInt3);
+
+	List.ForEach([](Test* pInt)
+		{
+			std::cout << pInt->Value << " ";
+		});
+
+	std::cout << std::endl;
+
+	List.Unlink(pInt2);
+
+	List.ForEach([](Test* pInt)
+		{
+			std::cout << pInt->Value << " ";
+		});
+
+	std::cout << std::endl;
+
+	List.ClearLink();
+
+	delete pInt1;
+	delete pInt2;
+	delete pInt3;
+
+	return 0;
+}
